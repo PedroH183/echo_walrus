19ea9422ed7b0d6e47bf1e1ac2f0c336fed962c0
PedroH183 <pedro_freires18@outlook.com>
2025-04-06 17:54:31 -0300

    refactor: clean up main function by removing unnecessary comments and imports

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ import os
 import logging
 
 from app.bot.telegram_bot import TelegramBot
-import asyncio
 
 def main():
-    """Main function to run the bot."""
-
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO
@@ -16,7 +13,6 @@ def main():
     if not token:
         raise ValueError("No token provided. Set the TELEGRAM_BOT_TOKEN environment variable.")
 
-    # Create and run the bot
     bot = TelegramBot(token)
     bot.run()
 
